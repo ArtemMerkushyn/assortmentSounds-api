4316dd3ce8cf9d34a33f73ed947a2ededa487bda
ArtemMerkushyn <merkushyndev13@gmail.com>
2023-12-18 18:42:01 +0200

    started the project

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+const PORT = 8080;
+
+//middleware
+app.use(cors()); //для того щоб можно було відправляти з різних ip, запроси до нашого серверу
+app.use(express.json()); //для  того щоб express розумів що дані з фронту будуть приходити у форматі json
+
+// routes
+
+async function start() {
+    try {
+        await mongoose.connect(
+                `mongodb+srv://merkushyndev13:merkushyndev13@cluster0.ll8ofmb.mongodb.net/`
+            );
+        app.listen(PORT, () => {
+            console.log(`server started on: http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
